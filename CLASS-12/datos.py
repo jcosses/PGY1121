b1b0a6a213957d76d365fb6f1b4f35173a27e699
jcosses <120616033+jcosses@users.noreply.github.com>
2023-05-24 14:52:58 -0400

    Archivo con validaciones incorporadas

--- CLASS-12/datos.py
+++ CLASS-12/datos.py
@@ -3,7 +3,13 @@
 print("DATOS PERSONALES")
 print("----------------\n")
 vNom = input("Ingrese su Nombre: ")
-vEdad = input("Ingrese su Edad: ")
+while True:
+    try:
+        vEdad = int(input("Ingrese su Edad: "))
+    except:
+        print("Valor no Corresponde")
+
+
 print("---------------------------")
 print(f"Su Nombres es: {vNom}")
 print(f"Su Edad es: {vEdad}")

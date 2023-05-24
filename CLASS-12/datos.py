2dd798839c681772a3e0e82fc941ac85be5ad743
jcosses <120616033+jcosses@users.noreply.github.com>
2023-05-24 14:33:59 -0400

    Archivo datos.py sin Validar

---
+++ CLASS-12/datos.py
@@ -0,0 +1,12 @@
+# EJEMPLO DE USO DE REPOSITORIO
+
+print("DATOS PERSONALES")
+print("----------------\n")
+vNom = input("Ingrese su Nombre: ")
+vEdad = input("Ingrese su Edad: ")
+print("---------------------------")
+print(f"Su Nombres es: {vNom}")
+print(f"Su Edad es: {vEdad}")
+
+print("Pgrama Finalizado....")
+
